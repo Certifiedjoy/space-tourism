cc90c2c58aeea691ae5ebe9a2bcbbdf69c5ae997
Khalid Alabi <81325840+DeanSft-dev@users.noreply.github.com>
2022-02-16 01:31:08 +0100

    desktop-view

---
+++ space-tourism-website-main/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        // nav: true,
+        // navText: ['Prev', 'Next'],
+        slideBy: 0,
+        items: 1,
+        stagePadding:20,
+    })
+})
